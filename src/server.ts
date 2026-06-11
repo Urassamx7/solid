947a7d2b817fee495e8f905a0ffb0ff8fa691945
Urassamx7 <marvin.lagas01234@gmail.com>
2026-06-11 22:47:27 +0200

    add biome configuration and update project settings

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import { app } from "./app";
-import { env } from "./utils/env";
+import { env } from '@/utils/env'
+import { app } from './app'
 
-app
-  .listen({
-    host: "0.0.0.0",
-    port: env.PORT,
-  })
-  .then((address) => {
-    console.log(`Server is running on ${address}`);
-  });
+app.listen({
+	host: '0.0.0.0',
+	port: env.PORT
+}).then((address) => {
+	console.log(`Server is running on ${address} 🚀`)
+})
